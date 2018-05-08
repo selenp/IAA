d2c5c31369b188b5037b6e88db1bfef1de24a158
lei <oizhaolei@gmail.com>
2018-05-08 20:51:16 +0800

    3rd party libraries

--- client/android/app/src/main/java/com/equipment/MainApplication.java
+++ client/android/app/src/main/java/com/equipment/MainApplication.java
@@ -3,6 +3,10 @@ package com.equipment;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativecomponent.barcode.RCTCapturePackage;
+import com.rnfs.RNFSPackage;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
+import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +26,11 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new RCTCapturePackage(),
+            new RNFSPackage(),
+            new RNDeviceInfo(),
+            new ReactNativeConfigPackage()
       );
     }
 
